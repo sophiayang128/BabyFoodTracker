ec676a90ee4d1dcb24da07def8730861529c9e3f
Sophia Tang <sophiatang@Sophias-Mac-Studio.local>
2025-07-02 10:39:10 -0700

    init commit

---
+++ BabyFoodTracker/BabyFoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFoodTrackerApp.swift
+//  BabyFoodTracker
+//
+//  Created by Sophia Tang on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BabyFoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyFoodTrackerTests/BabyFoodTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BabyFoodTrackerTests.swift
+//  BabyFoodTrackerTests
+//
+//  Created by Sophia Tang on 7/2/25.
+//
+
+import Testing
+
+struct BabyFoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
